332a8c596ac09d95e50ad23abcbe5582b60adf3d
[Github <[Github>
2022-11-03 03:23:51 +0900

    ^-^

--- chat/chat.controller.js
+++ chat/chat.controller.js
@@ -1,32 +1,95 @@
 const ChatsService = require('./chat.service');
+const { Users, Chats, ChatList, SalePosts } = require('../models');          //모델 데이터를 가져오고
+
 
 class ChatsController {
     chatsService = new ChatsService();
 
-    //새로운 채팅룸을 만든다.
-    createRoom= async(req, res, next) =>{
+    //새로운 채팅룸을 만든다. 정확히는 채팅하기 버튼을 누른 경우에 대한 것
+    startChat= async(req, res, next) =>{
+
         try{
+            //################################임시 데이터 영역 (로그인 기능이 구현되면 이부분을 수정)
+            // const { userId } = req.app.locals.user;     //채팅룸을 만들 사람의 id를 가져오고
+            // const { postId } = req.body;
+            const userId = 3
+            const postId = 1
+            //################################임시 데이터 영역 (로그인 기능이 구현되면 이부분을 수정)
+            const dup = await ChatList.findAll({where:{userId, postId}})
+            let CLID = 0
 
-            //################################################################
-            //const { userId } = req.app.locals.user;     //채팅룸을 만들 사람의 id를 가져오고
-            const userId = 2;//여기서 유저 아이디는 구매자 id다.
-            //################################################################
-
-            const postId = 1;//여기서 포스트 아이디는 상품id이며 이게 있으면 판매자 id도 쉽게 가져올 수 있다.
-
-            const createChattingRoomData = await this.chatsService.createRoom(userId, postId);
-            res.status(201).send({data: createChattingRoomData});          
+            if(dup[0]){         //이미 채팅방이 있다면 그 채팅방의 아이디를 가져온다
+                const dup_leng = dup.length
+                CLID = dup[dup_leng-1].dataValues.chatListId               
+            }else{              //채팅방이 없다면 만든 다음 그 아이디를 가져온다.
+                const createChattingRoomData = await this.chatsService.createRoom(userId, postId);
+                CLID = createChattingRoomData.dataValues.chatListId 
+            }
+            res.status(201).send({data: "채팅룸 아이디: " + CLID});  
+            return CLID        
         }catch(error){
             return res.status(500).send({ errorMessage:error.message});
         }
     };
 
+    //내가 가진 채팅 목록을 가져온다.
+    myRoom = async (req, res, next) =>{
+        try{
+            //################################임시 데이터 영역 (로그인 기능이 구현되면 이부분을 수정)
+            //const { userId } = req.app.locals.user; //유저 아이디를 가져오고
+            const userId = 3
+            //################################임시 데이터 영역 (로그인 기능이 구현되면 이부분을 수정)
+
+            const myChat = await ChatList.findAll({where:{userId}})
+            console.log(myChat)
+            return res.status(201).send({data: myChat});  
+        }catch(error){
+            return res.status(500).send({ errorMessage:error.message});
+        }
+    }
+
+    enteringRoom = async (req, res, next) =>{//바디에선 입장하고자한 채팅룸 아이디를 건네준다. 그리고 그곳에서 postId를 가져오고
+        try{
+            //################################임시 데이터 영역 (로그인 기능이 구현되면 이부분을 수정)
+            //const { userId } = req.app.locals.user; //유저 아이디를 가져오고
+            const userId = 5
+            //################################임시 데이터 영역 (로그인 기능이 구현되면 이부분을 수정)
+            const { chatListId } = req.params; //파람스에서 쳇방 아이디를 가져오고
+            const myInfo = await Users.findAll({where:{userId}})    //로그인 한 사람(채팅시작 버튼을 누른 사람)의 ID
+            const roomInfo = await ChatList.findAll({where:{chatListId}})//채팅방 정보를 가져옴
+            const post = roomInfo[0].dataValues.postId
+            const postInfo = await SalePosts.findAll({where:{postId:post}})     //게시글의 데이터
+            const postInfoData = postInfo[0].dataValues
+            const auth_user =  postInfoData.userId;
+            const authorInfo = await Users.findAll({where:{userId:auth_user}})
+            const authorInfoData = authorInfo[0].dataValues
+
+            const chatData = await Chats.findAll({where:{chatlistId: chatListId}})      //채팅데이터
+
+            const data = {
+                chatListId: chatListId,
+                title: postInfoData.title,
+                postImgUrl: postInfoData.postImgUrl,
+                price: postInfoData.price,
+                authorId: postInfoData.userId,             //포스트를 작성한 사람의 ID
+                authorName: authorInfoData.nickname,
+                authorProfileImage: authorInfoData.profileImage
+            };
+            
+            return  res.status(201).send({data: data, chatData:chatData});
+        }
+        catch(error){
+            return res.status(500).send({ errorMessage:error.message});
+        }
+    
+    }
+
     // //특정 채팅룸에서, 채팅룸에 있는 대화의 내용을 가져온다.
     getChats = async(req, res, next) => {
         try{
-        const {chatListId} = req.params;//포스트의 아이디를 가져와야 함
-        const chats = await this.chatsService.getChats(chatListId);//포스트서비스의 findAllPost를 사용
-        res.status(200).json({data:chats});//컨트롤러는 요청과 응답에 관여하니 응답만
+            const {chatListId} = req.params;//포스트의 아이디를 가져와야 함
+            const chats = await this.chatsService.getChats(chatListId);//포스트서비스의 findAllPost를 사용
+            res.status(200).json({data:chats});                     //컨트롤러는 요청과 응답에 관여하니 응답만
         }catch(error){
             return res.status(500).send({ errorMessage:error.message});
         }
@@ -34,7 +97,6 @@ class ChatsController {
 
     //채팅을 친다.
     createChats = async(req, res, next) =>{
-
         try{
             const { chatListId } = req.params;  //채팅을 작성할 채팅리스트
             const {message} =req.body;          //채팅의 내용
@@ -44,7 +106,7 @@ class ChatsController {
                 return;
             }   
             //################################################################
-            //const user=req.app.locals.user;                     //로그인중인 유저의 정보를 가져온다.
+            const user=req.app.locals.user;                     //로그인중인 유저의 정보를 가져온다.
             //const userId = user.userId                          //로그인 유저의 아이디를 가져옴
             const userId = 1;
             //################################################################
